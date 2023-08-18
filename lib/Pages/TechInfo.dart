6ceeed653898c70c366f1ca6f7b5455e37ddc408
tamish <tamisg99@gmail.com>
2023-08-18 20:14:53 +0300

    edit the colors of the table

--- lib/Pages/TechInfo.dart
+++ lib/Pages/TechInfo.dart
@@ -33,23 +33,71 @@ class _TechInfoState extends State<TechInfo> {
               // columnWidths:Map(),
 
               children: [
-                FullRow('Item', 'Pin no', 'GPIO Mode', 'Label'),
+                TableRow(
+                  decoration: BoxDecoration(
+          // backgroundBlendMode: BlendMode.colorDodge,
+          color: Colors.blueGrey.shade400,
+        ),
+                  children: [
+                    TableCell(
+      verticalAlignment: TableCellVerticalAlignment.middle,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text('Item',    
+          selectionColor: Colors.amber,
+          style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    TableCell(
+      verticalAlignment: TableCellVerticalAlignment.middle,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text('Pin no',    
+          selectionColor: Colors.amber,
+          style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    TableCell(
+      verticalAlignment: TableCellVerticalAlignment.middle,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text('GPIO Mode',    
+          selectionColor: Colors.amber,
+          style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    TableCell(
+      verticalAlignment: TableCellVerticalAlignment.middle,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text('Label',    
+          selectionColor: Colors.amber,
+          style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold,),
+        ),
+      ),
+    ),
+                  ]
+                  ),
+                // FullRow('Item', 'Pin no', 'GPIO Mode', 'Label'),
                 FullRow('In Pump', '21', 'O/P', '0'),
                 FullRow('Plant valve', '16', 'O/P', '2'),
                 FullRow('Drink valve', '5', 'O/P', '7'),
-                FullRow('Plant pump', '18', '1', '6'),
-                FullRow('Drink pump', '4', '1', '5'),
-                FullRow('Permit flow Sensor', '14', '1', '1'),
-                FullRow('Concentrate flow Sensor', '27', '1', '4'),
-                FullRow('Temperature sensor', '12', '1', '66'),
-                FullRow('TDS sensor', '35', '1', '8'),
-                FullRow('Servo', '1', '26', '22'),
-                FullRow('Trigger drink', '25', '1', '38'),
-                FullRow('Trigger plant', '2', '1', '83'),
-                FullRow('Echo drink', '34', '1', '48'),
-                FullRow('Echo plant', '32', '1', '84'),
-                FullRow('Voltage sensor', '36', '1', '55'),
-                FullRow('Current sensor', '33', '1', '11'),
+                FullRow('Plant pump', '18', 'O/P', '6'),
+                FullRow('Drink pump', '4', 'O/P', '5'),
+                FullRow('Permit flow Sensor', '14', 'I/P', '1'),
+                FullRow('Concentrate flow Sensor', '27', 'I/P', '4'),
+                FullRow('Temperature sensor', '12', 'I/P', '66'),
+                FullRow('TDS sensor', '35', 'I/P', '8'),
+                FullRow('Servo', '26', 'O/P', '22'),
+                FullRow('Trigger drink', '25', 'O/P', '38'),
+                FullRow('Trigger plant', '2', 'O/P', '83'),
+                FullRow('Echo drink', '34', 'I/P', '48'),
+                FullRow('Echo plant', '32', 'I/P', '84'),
+                FullRow('Voltage sensor', '36', 'I/P', '55'),
+                FullRow('Current sensor', '33', 'I/P', '11'),
                 FullRow('Vcc “5v”', '---', '---', '77'),
                 FullRow('Vcc “12v”', '---', '---', '50'),
                 FullRow('ground', '---', '---', '76&60'),
@@ -75,10 +123,12 @@ class _TechInfoState extends State<TechInfo> {
     return TableRow(
         decoration: BoxDecoration(
           // backgroundBlendMode: BlendMode.colorDodge,
-          color: Colors.blueGrey.shade400,
+          color: Colors.amber.shade100,
         ),
         children: [
+        
           RowCell(
+          
             content: a,
           ),
           RowCell(
@@ -110,7 +160,7 @@ class RowCell extends StatelessWidget { //// this class creat one cell
         child: Text(
           content,
           selectionColor: Colors.amber,
-          style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 12),
         ),
       ),
     );
