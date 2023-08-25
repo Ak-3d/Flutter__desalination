1294394c60ab3a661375c1d0afa6d1904fd56f46
maxandroid33 <yuosifmax309@gmail.com>
2023-08-25 04:51:47 +0300

    fix connection of app with esp32

--- lib/ForgroundService.dart
+++ lib/ForgroundService.dart
@@ -216,8 +216,8 @@ void onStart(ServiceInstance service) async {
     connectionHandler.sendWebsocket('check:${timer.tick}');
   });
 
-  connectDirectly('192.168.43.133');
-  // connect();
+  // connectDirectly('192.168.43.133');
+  connect();
 }
 
 void connect() async {
